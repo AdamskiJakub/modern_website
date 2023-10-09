34e392a30a9508a06e6df8a3644d8154cbb27e94
AdamskiJakub <105269736+AdamskiJakub@users.noreply.github.com>
2023-10-10 00:32:55 +0200

    Fixing billing and adding CardDeal

--- src/App.jsx
+++ src/App.jsx
@@ -29,8 +29,8 @@ const App = () => {
       <div className={`bg-primary ${styles.paddingX}${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Stats />
-          <Business /> <Billing /> <Testimonials /> <Clients /> <CTA />{" "}
-          <Footer />
+          <Business /> <Billing /> <CardDeal />
+          <Testimonials /> <Clients /> <CTA /> <Footer />
         </div>
       </div>
     </div>
